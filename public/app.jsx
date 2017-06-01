5d62e873dbe8957923448302a596476ee8ff35c3
Tom Shetler <tshet17@hotmail.com>
2017-06-01 18:51:38 -0400

    Final commit

--- public/app.jsx
+++ public/app.jsx
@@ -2,6 +2,7 @@ var GreeterMessage = React.createClass({
   render: function () {
     var name = this.props.name;
     var message = this.props.message;
+
     return (
       <div>
         <h1>Hello {name}!</h1>
@@ -15,22 +16,37 @@ var GreeterForm = React.createClass({
   onFormSubmit: function (e) {
     e.preventDefault();
 
+    var updates = {};
     var name = this.refs.name.value;
+    var message = this.refs.message.value;
 
     if (name.length > 0) {
       this.refs.name.value = '';
-      this.props.onNewName(name);
+      updates.name = name;
     }
+
+    if (message.length > 0) {
+      this.refs.message.value = '';
+      updates.message = message;
+    }
+
+    this.props.onNewState(updates);
   },
   render: function () {
     return (
       <form onSubmit={this.onFormSubmit}>
-        <input type="text" ref="name"/>
+      <div>
+        <input type="text" ref="name" placeholder="Enter name"/>
+      </div>
+    <div>
+        <textarea ref="message" placeholder="Enter message"></textarea>
+    </div>
         <button>Set Name</button>
       </form>
     );
   }
 });
+//////////////////////////////////////////
 
 var Greeter = React.createClass({
   getDefaultProps: function () {
@@ -41,22 +57,21 @@ var Greeter = React.createClass({
   },
   getInitialState: function () {
     return {
-      name: this.props.name
+      name: this.props.name,
+      message: this.props.message
     };
   },
-  handleNewName: function (name) {
-    this.setState({
-      name: name
-    })
+  handleNewState: function (updates) {
+    this.setState(updates);
   },
   render: function () {
     var name = this.state.name;
-    var message = this.props.message;
+    var message = this.state.message;
 
     return (
       <div>
         <GreeterMessage name={name} message={message}/>
-        <GreeterForm onNewName={this.handleNewName}/>
+        <GreeterForm onNewState={this.handleNewState}/>
 
       </div>
     );
